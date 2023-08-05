28578b711e45c799eca2ab2ad1f9ffbbab8c5cb6
Loomman1 <87032054+Loomman1@users.noreply.github.com>
2023-08-05 15:20:00 +0300

    pre-final

--- Tetris.cpp
+++ Tetris.cpp
@@ -1,6 +1,7 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 #include <SFML/Graphics.hpp>
 #include <SFML/OpenGL.hpp>
+#include <time.h>
 
 #include <iostream>
 
@@ -25,16 +26,26 @@ int figures[7][4] = {
     3,5,7,6,
     2,3,4,5,
 };
+
+bool check()
+{
+    for (int i = 0; i < 4; i++)
+        if (a[i].x < 0 || a[i].x >= N || a[i].y >= M)
+            return 0;
+        else if (field[a[i].y][a[i].x])
+            return 0;
+    return 1;
+}
 int main()
 {
+    srand(time(0));
     RenderWindow window(VideoMode(N * w, M * w), "Tetris");
     Texture t;
     t.loadFromFile("Paint/tiles.png" );
     Sprite tiles(t);
-    tiles.setTextureRect(IntRect(0, 0, w, w));
-    int dx = 0;
+    int dx = 0, colorNum=1;
     bool rotate = false;
-    float timer = 0, delay = 0.7;
+    float timer = 0, delay = 0.5;
     Clock clock;
     while (window.isOpen())
     {
@@ -56,9 +67,19 @@ int main()
                 else if (event.key.code == Keyboard::Left)
                     dx = -1;
         }
+        if (Keyboard::isKeyPressed(Keyboard::Down))
+            delay = 0.05;
         for (int i = 0; i < 4; i++)
         {
+            b[i] = a[i];
             a[i].x += dx;
+        }
+        if (!check())
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                a[i] = b[i];
+            }
         }
         if (rotate){
             Point p = a[1];
@@ -69,29 +90,71 @@ int main()
                 a[i].x = p.x - x;
                 a[i].y = p.y + y;
             }
+
+            if (!check())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    a[i] = b[i];
+                }
+            }
         }
         if (timer > delay)
         {
             for (int i = 0; i < 4; i++)
             {
+                b[i] = a[i];
                 a[i].y += 1;
-                timer = 0;
             }
+            if (!check())
+            {
+                for (int i = 0; i < 4; i++)
+                {
+                    field[b[i].y][b[i].x] = colorNum;
+                    colorNum = 1 + rand()%7;
+                    int n = rand()%7;
+                    for (int i = 0; i < 4; i++)
+                    {
+                        a[i].x = figures[n][i] % 2;
+                        a[i].y = figures[n][i] / 2;
+                    }
+                }
+            }
+            timer = 0;
         }
-        //comm
-        //commmamam
-        int n = 0;
-        if(a[0].x==0)
-        for (int i = 0; i < 4; i++)
+
+        int k = M - 1;
+        for (int i = M - 1; i > 0; i--)
         {
-            a[i].x = figures[n][i] % 2;
-            a[i].y = figures[n][i] / 2;
+            int count = 0;
+            for (int j = 0; j < N; j++)
+            {
+                if (field[i][j])count++;
+                field[k][j] = field[i][j];
+            }
+            if (count < N)
+                k--;
         }
+        
         dx = 0;
         rotate = false;
+        delay = 0.3;
         window.clear(Color::White);
+        for (int i = 0; i < M; i++)
+        {
+            for (int j = 0; j < N; j++)
+            {
+                if (field[i][j] == 0)
+                    continue;
+                tiles.setTextureRect(IntRect(field[i][j]*w, 0, w, w));
+                tiles.setPosition(j * w, i * w);
+                window.draw(tiles);
+            }
+
+        }
         for (int i = 0; i < 4; i++)
         {
+            tiles.setTextureRect(IntRect(colorNum*w, 0, w, w));
             tiles.setPosition(a[i].x * w, a[i].y * w);
             window.draw(tiles);
         }
